83a5d2e1eff870453ede01dabf86268a9979c6f4
nozwock <57829219+nozwock@users.noreply.github.com>
2025-06-16 11:57:30 +0530

    Format script using prettier

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,81 @@
 let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")]; //Pink from BetaMindy
 
-function sliderTable(table){
+function sliderTable(table) {
     let curSpeed = 0;
 
-    table.table(Tex.buttonEdge3, t => {
+    table.table(Tex.buttonEdge3, (t) => {
         t.name = "tc-slidertable";
         let timeSlider = new Slider(-8, 8, 1, false);
         timeSlider.setValue(0);
-        
-        let timeButton = t.button("[accent]x1", () => {
-            curSpeed++;
-            if (curSpeed > 2) curSpeed = -2; // Cycling back
-            curSpeed = Mathf.clamp(curSpeed, -2, 2);
 
-            timeSlider.setValue(curSpeed)
-        }).grow().width(10.5 * 8).get();
+        let timeButton = t
+            .button("[accent]x1", () => {
+                curSpeed++;
+                if (curSpeed > 2) curSpeed = -2; // Cycling back
+                curSpeed = Mathf.clamp(curSpeed, -2, 2);
+
+                timeSlider.setValue(curSpeed);
+            })
+            .grow()
+            .width(10.5 * 8)
+            .get();
         timeButton.margin(0);
         let lStyle = timeButton.getStyle();
         lStyle.up = Tex.pane;
         lStyle.over = Tex.flatDownBase;
         lStyle.down = Tex.whitePane;
-        
-        let resetButton = t.button(new TextureRegionDrawable(Icon.refresh), 24, () => timeSlider.setValue(0)).padLeft(6).get();
+
+        let resetButton = t
+            .button(new TextureRegionDrawable(Icon.refresh), 24, () => {
+                timeSlider.setValue(0);
+            })
+            .padLeft(6)
+            .get();
         resetButton.getStyle().imageUpColor = Pal.accent;
 
         t.add(timeSlider).padLeft(6).minWidth(200);
-        timeSlider.moved(v => {
+        timeSlider.moved((v) => {
             curSpeed = v;
             let speed = Math.pow(2, v);
-            Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
-            
+            Time.setDeltaProvider(() =>
+                Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed),
+            );
+
             Tmp.c1.lerp(cols, (timeSlider.getValue() + 8) / 16);
-            
+
             timeButton.setText(speedText(v));
         });
     });
     table.visibility = () => visibility();
 }
 
-
-function speedText(speed){
+function speedText(speed) {
     Tmp.c1.lerp(cols, (speed + 8) / 16);
     let text = "[#" + Tmp.c1.toString() + "]";
-    if(speed >= 0){
+    if (speed >= 0) {
         text += "x" + Math.pow(2, speed);
-    }else{
+    } else {
         text += "x1/" + Math.pow(2, Math.abs(speed));
     }
     return text;
 }
 
-function visibility(){
-    if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
-    if(!Vars.mobile) return true;
-    
+function visibility() {
+    if (!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
+    if (!Vars.mobile) return true;
+
     let input = Vars.control.input;
     return input.lastSchematic == null || input.selectPlans.isEmpty();
-}    
+}
 
-if(!Vars.headless){
+if (!Vars.headless) {
     Events.on(ClientLoadEvent, () => {
         let st = new Table();
         st.bottom().left();
         sliderTable(st);
         Vars.ui.hudGroup.addChild(st);
-        
-        if(Vars.mobile){
+
+        if (Vars.mobile) {
             st.moveBy(0, Scl.scl(46));
         }
     });
