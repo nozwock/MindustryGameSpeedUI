a06a967bfac5d92474caa7809598bc638c033738
nozwock <57829219+nozwock@users.noreply.github.com>
2025-06-16 12:29:43 +0530

    Refactor script for better readability

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,64 @@
-let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")]; //Pink from BetaMindy
+let pinkColor = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")]; //Pink from BetaMindy
 
-function sliderTable(table) {
-    let curSpeed = 0;
+function createTimeControlWidget(table) {
+    let currentGameSpeed = 0;
 
-    table.table(Tex.buttonEdge3, (t) => {
-        t.name = "tc-slidertable";
+    table.table(Tex.buttonEdge3, (table) => {
+        table.name = "tc-slidertable";
         let timeSlider = new Slider(-8, 8, 1, false);
         timeSlider.setValue(0);
 
-        let timeButton = t
+        let timeButton = table
             .button("[accent]x1", () => {
-                curSpeed++;
-                if (curSpeed > 2) curSpeed = -2; // Cycling back
-                curSpeed = Mathf.clamp(curSpeed, -2, 2);
+                currentGameSpeed++;
+                if (currentGameSpeed > 2) currentGameSpeed = -2; // Cycling back
+                currentGameSpeed = Mathf.clamp(currentGameSpeed, -2, 2);
 
-                timeSlider.setValue(curSpeed);
+                // log("Time button callback", curSpeed);
+                timeSlider.setValue(currentGameSpeed);
             })
             .grow()
             .width(10.5 * 8)
             .get();
         timeButton.margin(0);
-        let lStyle = timeButton.getStyle();
-        lStyle.up = Tex.pane;
-        lStyle.over = Tex.flatDownBase;
-        lStyle.down = Tex.whitePane;
+        {
+            let style = timeButton.getStyle();
+            style.up = Tex.pane;
+            style.over = Tex.flatDownBase;
+            style.down = Tex.whitePane;
+        }
 
-        let resetButton = t
+        let resetButton = table
             .button(new TextureRegionDrawable(Icon.refresh), 24, () => {
+                // log("Reset button callback", 0);
                 timeSlider.setValue(0);
             })
             .padLeft(6)
             .get();
         resetButton.getStyle().imageUpColor = Pal.accent;
 
-        t.add(timeSlider).padLeft(6).minWidth(200);
+        table.add(timeSlider).padLeft(6).minWidth(200);
         timeSlider.moved((v) => {
-            curSpeed = v;
-            let speed = Math.pow(2, v);
-            Time.setDeltaProvider(() =>
-                Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed),
-            );
+            // log("Slider callback", v);
+            currentGameSpeed = v;
+            setGameSpeed(v);
 
-            Tmp.c1.lerp(cols, (timeSlider.getValue() + 8) / 16);
-
-            timeButton.setText(speedText(v));
+            Tmp.c1.lerp(pinkColor, (timeSlider.getValue() + 8) / 16);
+            timeButton.setText(toSpeedText(v));
         });
     });
-    table.visibility = () => visibility();
+    table.visibility = () => getGUIVisibility();
 }
 
-function speedText(speed) {
-    Tmp.c1.lerp(cols, (speed + 8) / 16);
+function setGameSpeed(speed) {
+    let speed = Math.pow(2, speed);
+    Time.setDeltaProvider(() =>
+        Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed),
+    );
+}
+
+function toSpeedText(speed) {
+    Tmp.c1.lerp(pinkColor, (speed + 8) / 16);
     let text = "[#" + Tmp.c1.toString() + "]";
     if (speed >= 0) {
         text += "x" + Math.pow(2, speed);
@@ -60,7 +68,7 @@ function speedText(speed) {
     return text;
 }
 
-function visibility() {
+function getGUIVisibility() {
     if (!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
     if (!Vars.mobile) return true;
 
@@ -68,15 +76,16 @@ function visibility() {
     return input.lastSchematic == null || input.selectPlans.isEmpty();
 }
 
+// Register mod
 if (!Vars.headless) {
     Events.on(ClientLoadEvent, () => {
-        let st = new Table();
-        st.bottom().left();
-        sliderTable(st);
-        Vars.ui.hudGroup.addChild(st);
+        let timeControlTable = new Table();
+        timeControlTable.bottom().left();
+        createTimeControlWidget(timeControlTable);
+        Vars.ui.hudGroup.addChild(timeControlTable);
 
         if (Vars.mobile) {
-            st.moveBy(0, Scl.scl(46));
+            timeControlTable.moveBy(0, Scl.scl(46));
         }
     });
 }
